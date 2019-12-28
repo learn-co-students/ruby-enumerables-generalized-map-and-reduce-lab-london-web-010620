18451472f32bbdb574afd8372855920eaf305ab8
Simon Tucker <sjtucker28@gmail.com>
2019-12-28 14:23:43 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# Your Code Here
+#map
+# map returns an Array
+def map(array)
+  new = []
+  i = 0 
+  
+  while i < array.length do
+    new.push(yield(array[i]))
+    i += 1 
+  end
+  new
+end
+
+
+#reduce
+#reduce returns a value
+
+def reduce(array, starting_point  = nil)
+  new = []
+  
+  if starting_point
+    total = starting_point
+    i = 0
+  else 
+    total = array[0]
+    i = 1 
+  end
+  
+  while i < array.length do
+    total  = yield(total, array[i])
+    i += 1
+  end
+  total 
+end
+
+
+  
